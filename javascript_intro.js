24a9229b61daac7550657ac57acbb036675c2520
Egbuna Chinedu Victor <egbunachineduvictor@gmail.com>
2022-06-24 02:52:19 +0100

    submission

---
+++ javascript_intro.js
@@ -0,0 +1,7 @@
+var name = 'Egbuna Chinedu Victor';
+var height = '6.5 ft';
+const country = 'Nigeria';
+
+console.log('Name: ', name);
+console.log('Height: ', height);
+console.log('Country: ', height)
